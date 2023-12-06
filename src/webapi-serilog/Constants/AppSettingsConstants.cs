45f42ec202b31c75730b499a21728ee6929e9e97
Shazni Shiraz <shaznishiraz+github@gmail.com>
2023-12-06 17:34:33 +0530

    Create AppSettingsConstants.cs

---
+++ src/webapi-serilog/Constants/AppSettingsConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace webapi_serilog.Constants;
+
+public class AppSettingsConstants
+{
+    public const string AppInsightInstrumentationKeyForWebSites = "APPINSIGHTS_INSTRUMENTATIONKEY";
+    public const string AppInsightConnectionStringEnvironmentVariable = "APPLICATIONINSIGHTS_CONNECTION_STRING";
+    public const string LogLevel = nameof(LogLevel);
+    public const string Default = nameof(Default);
+    public const string IsUseDefaultLogger = nameof(IsUseDefaultLogger);
+}
